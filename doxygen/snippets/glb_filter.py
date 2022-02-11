4ef131f241eaef3051e9c251763ae95098999c3b
MMS <miro@quantum-leaps.com>
2022-02-11 10:14:39 -0500

    7.0.0
    restored the doxygen directory

---
+++ doxygen/snippets/glb_filter.py
@@ -0,0 +1,11 @@
+glb_filter()           # clears all global filters
+
+glb_filter(GRP_ALL)    # sets all global filters
+
+glb_filter(GRP_SM, GRP_AO)   # sets SM-group and AO-group
+
+glb_filter(GRP_ALL, -GRP_SC)  # sets all global filters, but clears the SC-group
+
+glb_filter(GRP_QF, "-QS_QF_TICK")  # sets the QF-group, but clears the QS_QF_TICK filter
+
+glb_filter(GRP_AO, 78)` # sets the AO-group and the QS record 78
---
+++ doxygen/snippets/loc_filter.py
@@ -0,0 +1,13 @@
+loc_filter(IDS_ALL) # enables all QS-IDs
+
+loc_filter(-IDS_ALL) # disables all QS-IDs
+
+loc_filter(IDS_AO)   # enables all active objcts
+
+loc_filter(IDS_AO, -6)   # enables all active objcts,
+                         # but disables AO with priority 6
+
+
+loc_filter(IDS_AO, IDS_EP)   # enables all active objects and event pools
+
+loc_filter(IDS_AP)       # enables all app-specific QS_IDs
