a8689203cc9624a2c5bec4f1d07e3da16d0cb678
MMS <miro@quantum-leaps.com>
2024-11-14 12:35:16 -0500

    8.0.0

--- qspy/lint-plus/make.bat
+++ qspy/lint-plus/make.bat
@@ -3,23 +3,11 @@
 :: Batch script for linting QP/C with PC-Lint-Plus2
 :: Copyright (C) 2005 Quantum Leaps, LLC. All rights reserved.
 ::
-:: SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-QL-commercial
+:: This software is licensed under the terms of the Quantum Leaps
+:: QSPY SOFTWARE TRACING HOST UTILITY SOFTWARE END USER LICENSE.
+:: Please see the file LICENSE-qspy.txt for the complete license text.
 ::
-:: This software is dual-licensed under the terms of the open source GNU
-:: General Public License version 3 (or any later version), or alternatively,
-:: under the terms of one of the closed source Quantum Leaps commercial
-:: licenses.
-::
-:: The terms of the open source GNU General Public License version 3
-:: can be found at: <www.gnu.org/licenses/gpl-3.0>
-::
-:: The terms of the closed source Quantum Leaps commercial licenses
-:: can be found at: <www.state-machine.com/licensing>
-::
-:: Redistributions in source code must retain this top-level comment block.
-:: Plagiarizing this software to sidestep the license obligations is illegal.
-::
-:: Contact information:
+:: Quantum Leaps contact information:
 :: <www.state-machine.com>
 :: <info@state-machine.com>
 :: ===========================================================================
@@ -45,7 +33,7 @@ set LINTFLAGS=-i64bit -i%GCC_INC% -dQSPY_APP options.lnt %1 %2 %3 %4
 @del *.log
 
 :: linting -------------------------------------------------------------------
-%PCLP% -os(lint.log) std.lnt %LINTFLAGS%  ..\source\*.c
+%PCLP% -os(lint.log) std.lnt %LINTFLAGS%  ..\priv_src\*.c
 
 :end
 @endlocal
